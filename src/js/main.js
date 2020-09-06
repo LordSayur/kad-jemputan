21532dfe431fb16dd0e968c091e3a5093f1e48b2
Lord Sayur <umar_317@hotmail.com>
2020-09-06 17:17:03 +0800

    Run animate function after p5 finish set up

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-import '../scss/main.scss';
 import p5 from 'p5/lib/p5.min.js';
 
 let s = (sk) => {
@@ -9,6 +8,7 @@ let s = (sk) => {
 	sk.setup = () => {
 		sk.createCanvas(window.innerWidth, document.documentElement.scrollHeight);
 		sk.background(40);
+		addAnimateCss();
 	};
 
 	sk.draw = () => {
@@ -18,3 +18,23 @@ let s = (sk) => {
 };
 
 const P5 = new p5(s, 'draw');
+
+const addAnimateCss = () => {
+	const border = document.querySelector('#welcomeBorder');
+	const omar = document.querySelector('#Omar');
+	const and = document.querySelector('#and');
+	const amirah = document.querySelector('#Amirah');
+	const welcomeNote = document.querySelector('#welcomeNote');
+	const hajat = document.querySelector('#hajat');
+
+	document.querySelector('#kad').classList.remove('hide');
+
+	border.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-2s');
+	omar.classList.add('animate__animated', 'animate__backInLeft', 'animate__delay-2s');
+	and.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-2s');
+	amirah.classList.add('animate__animated', 'animate__backInRight', 'animate__delay-2s');
+	welcomeNote.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-4s');
+	hajat.classList.add('animate__animated', 'animate__fadeInUp', 'animate__delay-4s');
+};
+
+import '../scss/main.scss';
