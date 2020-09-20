41741a60e52edd504699774a2a638f23bf3606be
Lord Sayur <umar_317@hotmail.com>
2020-09-20 12:21:41 +0800

    Update animation

--- src/js/main.js
+++ src/js/main.js
@@ -20,19 +20,19 @@ let s = (sk) => {
 const P5 = new p5(s, 'draw');
 
 const addAnimateCss = () => {
-	const border = document.querySelector('#welcomeBorder');
+	const border = document.querySelector('.border');
 	const omar = document.querySelector('#Omar');
 	const and = document.querySelector('#and');
 	const amirah = document.querySelector('#Amirah');
-	const welcomeNote = document.querySelector('#welcomeNote');
+	const welcomeNote = document.querySelector('#hajat-title');
 	const hajat = document.querySelector('#hajat');
 
-	document.querySelector('#kad').classList.remove('hide');
+	document.querySelector('#kad').classList.remove('cover');
 
 	border.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-2s');
-	omar.classList.add('animate__animated', 'animate__backInLeft', 'animate__delay-2s');
+	omar.classList.add('animate__animated', 'animate__fadeInRight', 'animate__delay-2s');
 	and.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-2s');
-	amirah.classList.add('animate__animated', 'animate__backInRight', 'animate__delay-2s');
+	amirah.classList.add('animate__animated', 'animate__fadeInLeft', 'animate__delay-2s');
 	welcomeNote.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-4s');
 	hajat.classList.add('animate__animated', 'animate__fadeInUp', 'animate__delay-4s');
 };
